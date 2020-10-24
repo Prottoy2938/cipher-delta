9fb85f9b484f43ce67f320095224c19363615780
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-10-24 19:13:18 +0600

    Added chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "../global.styles.css";
+import { ChakraProvider } from "@chakra-ui/core";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ChakraProvider>
+        <Component {...pageProps} />{" "}
+      </ChakraProvider>{" "}
+    </>
+  );
 }
--- src/components/handle-output/handle-output.tsx
+++ src/components/handle-output/handle-output.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Props } from "./handle-output.model";
 import styles from "./handle-output.module.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { IconButton, useToast, Box } from "@chakra-ui/core";
+import { CopyIcon } from "@chakra-ui/icons";
 
 const substituteContent = (userContent, skip = 1) => {
   const engAlphabets = "abcdefghijklmnopqrstuvwxyz";
@@ -32,11 +35,33 @@ const substituteContent = (userContent, skip = 1) => {
 
 const HandleOutput: React.FC<Props> = (props: Props) => {
   const { userContent } = props;
+  const content = substituteContent(userContent);
+  const toast = useToast();
+
+  const triggerCopyToast = (): void => {
+    toast({
+      position: "bottom-right",
+      status: "success",
+      description: "Copied",
+    });
+  };
   return (
-    <>
-      <button className={styles.copyBtn}>copy</button>
-      <div className={styles.container}>{substituteContent(userContent)}</div>
-    </>
+    <div className={styles.container}>
+      <CopyToClipboard text={content}>
+        <IconButton
+          aria-label="copy result"
+          float="right"
+          ml="-53px"
+          mt="-33px"
+          size="sm"
+          borderBottomLeftRadius={0}
+          borderBottomRightRadius={0}
+          onClick={triggerCopyToast}
+          icon={<CopyIcon />}
+        />
+      </CopyToClipboard>
+      <div className={styles.content}>{content}</div>
+    </div>
   );
 };
 
